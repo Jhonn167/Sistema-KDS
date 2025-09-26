e52a2a693d5613940d79320cc54b4f7d220c4725
Juan Garcia <112785336+Jhonn167@users.noreply.github.com>
2025-09-26 14:05:35 -0600

    fix: Corregir URLs de producción para Vercel

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // --- CONFIGURACIÓN DE CORS FINAL Y ROBUSTA ---
 const allowedOrigins = [
   "http://localhost:4200",
-  "https://sistema-kds-git-main-jhonn167s-projects.vercel.app" // URL de Vercel
+  "https://sistema-kds.vercel.app" // URL de Vercel
 ];
 const corsOptions = {
   origin: function (origin, callback) {
